06fc5c4d58387c27173596eb3e460a39fdf29b76
ohji <ohproji@naver.com>
2022-04-27 19:00:36 +0900

    0427

--- day01/mytest03.py
+++ day01/mytest03.py
@@ -7,3 +7,17 @@ num = int(input("출력하고 싶은 단수를 입력하세요"))
 
 for i in range(1,10) :
     print(num, "*", i, "=", num*i)
+
+
+    #print("{}*{}={}".format(num,i,num*i))
+
+print("{}*{}={}".format(num,1,num*1))
+print("{}*{}={}".format(num,2,num*2))
+print("{}*{}={}".format(num,3,num*3))
+print("{}*{}={}".format(num,4,num*4))
+print("{}*{}={}".format(num,5,num*5))
+print("{}*{}={}".format(num, 6, num * 6))
+print("{}*{}={}".format(num, 7, num * 7))
+print("{}*{}={}".format(num, 8, num * 8))
+print("{}*{}={}".format(num, 9, num * 9))
+
